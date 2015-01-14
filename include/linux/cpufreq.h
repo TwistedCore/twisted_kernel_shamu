3bcd6f92ad45e59e03aafc233809450a84f6dc56
RIPng <matthewalex@outlook.com>
2015-01-14 12:42:15 -0500

    cpufreq: intelliactive: added new @faux123 governor
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile
    #	include/linux/cpufreq.h
    
    (cherry picked from commit 10754966035720d9ffa8441f3fac588600f943f5)
    
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -421,6 +421,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #endif
 
 /*********************************************************************
